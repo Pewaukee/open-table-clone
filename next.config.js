613567c502d5631587937a01c3f678534401b2d6
Karthik Shankar <misterkar6@gmail.com>
2023-07-15 19:35:46 -0700

    image domain configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    /* add configuration for loading images in <Image> component */
+    images: {
+        domains: ['resizer.otstatic.com']
+    }
+}
 
 module.exports = nextConfig
